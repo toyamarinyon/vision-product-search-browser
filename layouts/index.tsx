8f0ce11f48d2098145dc92dbf44f86cca0aa53e0
toyamarinyon <toyamarinyon@gmail.com>
2022-05-17 08:46:32 +0900

    search form

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -6,12 +6,53 @@ interface LayoutProps {
   children: React.ReactNode
 }
 export function Layout({ children }: LayoutProps) {
+  const [productName, setProductName] = useState('')
   return (
     <div className="h-screen flex flex-col overflow-y-hidden">
       <header className="px-8 py-4 border-b">
-        <h1 className="text-xl font-bold text-gray-800">
-          Vision Product Search Data Browser
-        </h1>
+        <div className="flex space-x-8">
+          <h1 className="text-xl font-bold text-gray-800">
+            Vision Product Search Data Browser
+          </h1>
+          <form
+            className="relative flex items-center"
+            onSubmit={(e) => {
+              e.preventDefault()
+              if (productName == '') {
+                alert('Please enter a product name')
+                return
+              }
+              window.location.href = `/products/${productName}`
+            }}
+          >
+            <input
+              id="direct"
+              className="px-2 pr-8 py-1 rounded text-sm space-x-2 border w-72 bg-gray-50"
+              placeholder="Fill in product name and open it"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+
+            <button className="absolute right-2">
+              <div className="border rounded px-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={1}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+                  />
+                </svg>
+              </div>
+            </button>
+          </form>
+        </div>
       </header>
       <main className="flex flex-1 h-full">
         <div className="flex w-full">
@@ -47,7 +88,6 @@ function NavigationLink({ linkTo, label }: NavigationLinkProps) {
   useEffect(() => {
     setCurrentPage(location.pathname.startsWith(linkTo))
   }, [linkTo])
-  ;('hover:bg-gray-200')
   return (
     <Link href={linkTo}>
       <a
